2f115f4d6d3619ac1d7e7b10e7ce2d88949e7c08
Keep-Going-HyeonSoo <chs98105@gmail.com>
2020-05-09 10:35:31 +0000

    App - 함수를 이용해서 정리 정돈하기

--- main.js
+++ main.js
@@ -2,6 +2,35 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url'); // url 모듈을 변수에 저장
 
+function templateHTML(title, list, body){
+    return `
+            <!doctype html>
+            <html>
+            <head>
+                <title>WEB1 - ${title}</title>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1><a href="/">WEB</a></h1>
+                ${list}
+                ${body}
+            </body>
+          </html>
+          `;
+}
+
+function templateList(filelist){
+    var list = '<ul>';
+    var i=0;
+    while(i < filelist.length){
+        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+        i++;
+    }
+    list += '</ul>';
+    
+    return list;
+}
+
 var app = http.createServer(function(request,response){
     var _url = request.url;
     // _url(request.url) 에는 path 이하 주소가 들어감.
@@ -18,13 +47,7 @@ var app = http.createServer(function(request,response){
         // data 폴더 안에 있는 파일목록(filelist)을 배열형식으로 불러옴.
         // filelist 를 동적으로 표현하기 위해 list 라는 변수 설정
         fs.readdir('./data', function(error, filelist){
-            var list = '<ul>';
-            var i=0;
-            while(i < filelist.length){
-                list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-                i++;
-            }
-            list += '</ul>';
+            var list = templateList(filelist);   
         
             fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
                 if(queryData.id === undefined){ // 쿼리스트링이 없다면.. (= 홈페이지(WEB)에 접속했다면..)
@@ -37,23 +60,9 @@ var app = http.createServer(function(request,response){
                     var title = queryData.id;
                 }
           
-                var template = `
-                <!doctype html>
-                <html>
-                <head>
-                    <title>WEB1 - ${title}</title>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1><a href="/">WEB</a></h1>
-                    ${list}
-                    <h2>${title}</h2>
-                    <p>${description}</p>
-                </body>
-              </html>
-              `;
-              response.writeHead(200);
-              response.end(template);
+                var template = templateHTML(title,list,`<h2>${title}</h2>${description}`);
+                response.writeHead(200);
+                response.end(template);
             });
         });
     } 
