1793b8a5ae0a1fe651de5bb36c740996c579898d
Keep-Going-HyeonSoo <chs98105@gmail.com>
2020-05-21 13:58:02 +0000

    MySQL로 상세보기 구현

--- main.js
+++ main.js
@@ -58,6 +58,7 @@ var app = http.createServer(function(request,response){
     //  ex ) http://localhost:3000/?id=HTML => request.url == /?id=HTML
     var queryData = url.parse(_url,true).query;
     // queryData 에는 쿼리스트링이 객체타입으로 저장됨
+    // { id : 'HTML' }
      
     
     //console.log(url.parse(_url,true));
@@ -118,6 +119,7 @@ var app = http.createServer(function(request,response){
         
         // 쿼리스트링의 id 값이 없을 때 ( = 메인페이지에 접속했을 때)
         if(queryData.id === undefined){ 
+            // 목록들 출력하는 기능
             db.query(`SELECT * FROM topic`, function(error,topics){
                 //console.log(topics);
                 var title = 'Welcome';
@@ -131,6 +133,38 @@ var app = http.createServer(function(request,response){
                 response.end(html);
             });
         }
+        
+        // 쿼리스트링의 id 값이 있을 때 ( = 컨텐츠페이지에 접속했을 때)
+        // ( ex. ?id=HTML,CSS,JavaScript ) 
+        else{
+            
+            db.query(`SELECT * FROM topic`, function(error,topics){ // 목록 출력
+                if(error){
+                    throw error;
+                }
+                db.query(`SELECT * FROM topic WHERE id=?`,[queryData.id],function(error2,topic){
+                   if(error2){
+                       throw error2;
+                   } 
+                   
+                    var title = topic[0].title;
+                    var description = topic[0].description;
+                    var list = template.List(topics); // topics : 전체 목록 데이터가 있는 테이블
+                    var html = template.HTML(title,list,
+                                `<h2>${title}</h2>${description}`,
+                                `<a href="/create">create</a>
+                                <a href="/update?id=${queryData.id}">update</a>
+                                <form action="/delete_process" method="post" 
+                                        onsubmit='return confirm("삭제하시겠습니까?")'>
+                                  <input type="hidden" name="id" value="${queryData.id}">
+                                  <input type="submit" value="delete">
+                                </form>`);
+                    response.writeHead(200);
+                    response.end(html);
+                    });
+
+                });
+            }
     }
     
     else if(pathname === '/create'){ // 접속경로가 create 일 때..
