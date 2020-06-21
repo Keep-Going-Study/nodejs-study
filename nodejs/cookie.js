cc5f51a0a1c9a296fe8bba11e9e738923a91de3f
Keep-Going-HyeonSoo <chs98105@gmail.com>
2020-06-21 08:01:51 +0000

    쿠키 읽기

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,7 +1,17 @@
 var http = require('http');
+var cookie = require('cookie');
+
 http.createServer(function(request, response){
-     response.writeHead(200, {
-         'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
-     });
+     //console.log(request.headers.cookie); // 쿠키가 문자열로 저장
+     
+     if(request.headers.cookie !== undefined){ // request 에 쿠키가 있을 시, 서버에서 읽는다. 
+          var cookies = cookie.parse(request.headers.cookie); // cookies에는 쿠키가 객체로 저장
+     }
+     
+     else{
+          response.writeHead(200, {
+              'Set-Cookie':['yummy_cookie=choco', 'tasty_cookie=strawberry']
+          });
+     }
     response.end('Cookie!!');
 }).listen(8080);
